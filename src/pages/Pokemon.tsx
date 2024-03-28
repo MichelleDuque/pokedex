62f822e87de82c721574d9e1c2ca5588a2e8ac28
MichelleDuque <michelled_1994@hotmail.com>
2024-03-28 14:36:10 -0400

    made fetch with api to get pokemon specific data

--- src/pages/Pokemon.tsx
+++ src/pages/Pokemon.tsx
@@ -1,12 +1,35 @@
 import { useNavigate, useParams } from "react-router-dom";
 import PokeballImg from "../assets/pokeball.png";
-import BulbasaurImg from "../assets/bulbasaur.gif";
 import Footer from "../components/Footer";
 import styles from "./pokemon.module.css"
+import { useEffect, useState } from "react";
+import { PokemonDetails } from "../types/types";
+import { fetchPokemon } from "../api/fetchPokemon";
+import LoadingScreen from "../components/LoadingScreen";
+import { waitFor } from "../utils/utils";
 
 const Pokemon = () => {
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [pokemon, setPokemon] = useState<PokemonDetails>();
   const { name } = useParams();
   const navigate = useNavigate();
+
+  useEffect(() =>{
+    async function getPokemon(){
+      setIsLoading(true);
+      await waitFor(500);
+      const fetchedPokemon = await fetchPokemon(name as string);
+      setPokemon(fetchedPokemon)
+      setIsLoading(false);
+    }
+    getPokemon();
+
+}, [name])
+
+if (isLoading || !pokemon){
+  return <LoadingScreen />;
+}
 
   return (
   <>
@@ -17,14 +40,14 @@ const Pokemon = () => {
   </button>
   <div className={styles.pokemon}>
     <main className={styles.pokemonInfo}>
-      <div className={styles.pokemonTitle}>{name?.toUpperCase()}</div>
-      <div>Nr. 001</div>
+      <div className={styles.pokemonTitle}>{pokemon?.name?.toUpperCase()}</div>
+      <div>Nr. {pokemon?.id}</div>
       <div>
-        <img src={BulbasaurImg} alt="Bulbasaur" className={styles.pokemonInfoImg}/>
+        <img src={pokemon?.imgSrc} alt={pokemon?.name} className={styles.pokemonInfoImg}/>
       </div>
-      <div>HP: 000</div>
-      <div>Attack: 000</div>
-      <div>Defense: 000</div>
+      <div>HP: {pokemon?.hp}</div>
+      <div>Attack: {pokemon?.attack}</div>
+      <div>Defense: {pokemon?.defense}</div>
     </main>
   </div>
   <Footer />
